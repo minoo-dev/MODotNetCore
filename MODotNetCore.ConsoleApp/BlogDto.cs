6d308e9bc05b0ec0b0dfa8e50fde43c088f9e3fa
Min Oo <minoo.dev@gmail.com>
2024-04-28 22:16:41 +0630

    Add Blog DTO

---
+++ MODotNetCore.ConsoleApp/BlogDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MODotNetCore.ConsoleApp;
+
+public class BlogDto
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
