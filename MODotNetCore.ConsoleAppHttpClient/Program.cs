8b218c26a08fce3b1357bfca7d4d404bc0600d46
Min Oo <minoo.dev@gmail.com>
2024-05-27 23:54:45 +0630

    JSON to C# Objects Example

---
+++ MODotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonStr = await File.ReadAllTextAsync("data.json");
+
+//JSON to C#
+var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+
+//Console.WriteLine(jsonStr);
+
+foreach (var question in model.questions)
+{
+    Console.WriteLine(question.questionNo);
+}
+
+Console.ReadLine();
+
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
