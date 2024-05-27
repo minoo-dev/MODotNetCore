3733e5a3916d45be26ad1b19b8b9bdf6d61b3885
Min Oo <minoo.dev@gmail.com>
2024-05-28 00:46:08 +0630

    Refector JSON to C# Example

---
+++ MODotNetCore.ConsoleAppHttpClient/JsonToObjectExample.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace MODotNetCore.ConsoleAppHttpClient
+{
+    internal class JsonToObjectExample
+    {
+        public async Task GetJsonDataAsync()
+        {
+            string jsonStr = await File.ReadAllTextAsync("data.json");
+
+            //JSON to C#
+            var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+
+            //Console.WriteLine(jsonStr);
+
+            foreach (var question in model.questions)
+            {
+                Console.WriteLine(question.questionNo);
+            }
+        }
+    }
+}
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
